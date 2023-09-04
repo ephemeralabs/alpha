cf0377097627de22c9e6f0b6a08cbe71eed361bf
Eduardo Ponz <eduardoponz@eprosima.com>
2023-09-04 22:37:38 +0200

    Use constants in loop()
    Signed-off-by: Eduardo Ponz <eduardoponz@eprosima.com>

--- resources/arduino/ring_effects/ring_effects.ino
+++ resources/arduino/ring_effects/ring_effects.ino
@@ -4,9 +4,16 @@
 
 #include "NeoPixelRing.hpp"
 
-constexpr uint16_t NUM_PIXELS     = 8;
-constexpr int16_t  NEOPIXEL_PIN   = 3;
-constexpr uint8_t  MAX_BRIGHTNESS = ephemera::alpha::NeoPixelRing::max_brightness();
+/* Ring config */
+constexpr uint16_t NUM_PIXELS = 8;
+constexpr int16_t NEOPIXEL_PIN = 3;
+constexpr uint8_t MAX_BRIGHTNESS = ephemera::alpha::NeoPixelRing::max_brightness();
+
+/* Effects config */
+constexpr size_t EFFECT_ITERATIONS = 3;
+constexpr unsigned long CAROUSEL_DELAY = 100;
+constexpr unsigned long BLINK_DELAY = 800;
+constexpr unsigned long FADE_DELAY = 3000;
 
 void setup()
 {
@@ -19,21 +26,21 @@ void loop()
     ephemera::alpha::NeoPixelRing ring(NUM_PIXELS, NEOPIXEL_PIN, MAX_BRIGHTNESS);
 
     // Three iterations of green carousel
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < EFFECT_ITERATIONS; i++)
     {
-        ring.carousel(100);
+        ring.carousel(CAROUSEL_DELAY);
     }
 
     // Three iterations of red blinking
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < EFFECT_ITERATIONS; i++)
     {
-        ring.blink(800);
+        ring.blink(BLINK_DELAY);
     }
 
     // Three iterations of red fading in and out
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < EFFECT_ITERATIONS; i++)
     {
-        ring.fade_in(2500);
-        ring.fade_out(2500);
+        ring.fade_in(FADE_DELAY);
+        ring.fade_out(FADE_DELAY);
     }
 }
