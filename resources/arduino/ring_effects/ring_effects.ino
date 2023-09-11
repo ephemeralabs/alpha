2c853bb383cd61014fd757f0fd12e70d0f069457
Eduardo Ponz <eduardoponz@eprosima.com>
2023-09-11 20:48:58 +0200

    Add connection diagram
    Signed-off-by: Eduardo Ponz <eduardoponz@eprosima.com>

--- resources/arduino/ring_effects/ring_effects.ino
+++ resources/arduino/ring_effects/ring_effects.ino
@@ -2,6 +2,50 @@
  * Copyright (C) 2023, Ephemera Labs
  */
 
+/*
+ * http://busyducks.com/ascii-art-arduinos
+ *
+ *                                +-----+
+ *   +----[PWR]-------------------| USB |--+
+ *   |                            +-----+  |
+ *   |         GND/RST2  [ ][ ]            |
+ *   |       MOSI2/SCK2  [ ][ ]  A5/SCL[ ] |   C5
+ *   |          5V/MISO2 [ ][ ]  A4/SDA[ ] |   C4
+ *   |                             AREF[ ] |
+ *   |                              GND[ ] |
+ *   | [ ]N/C                    SCK/13[ ] |   B5
+ *   | [ ]IOREF                 MISO/12[ ] |   .
+ *   | [ ]RST                   MOSI/11[ ]~|   .
+ *   | [ ]3V3    +---+               10[ ]~|   .
+ *   | [ ]5v    -| A |-               9[ ]~|   .
+ *   | [ ]GND   -| R |-               8[ ] |   B0
+ *   | [ ]GND   -| D |-                    |
+ *   | [ ]Vin   -| U |-               7[ ] |   D7
+ *   |          -| I |-               6[ ]~|   .
+ *   | [ ]A0    -| N |-               5[ ]~|   .
+ *   | [ ]A1    -| O |-               4[ ] |   .
+ *   | [ ]A2     +---+           INT1/3[R]~|   .
+ *   | [ ]A3                     INT0/2[B] |   .
+ *   | [ ]A4/SDA  RST SCK MISO     TX>1[ ] |   .
+ *   | [ ]A5/SCL  [ ] [ ] [ ]      RX<0[ ] |   D0
+ *   |            [ ] [ ] [ ]              |
+ *   |  UNO_R3    GND MOSI 5V  ____________/
+ *    \_______________________/
+ *
+ * R: NeoPixel Ring          B: Change effect button
+ *
+ * +-------+                        +-----------+
+ * |       |                        |           |
+ * |   RS [ ]---D3           GND---[ ] BG   B2 [ ]
+ * |       |                        |           |
+ * |   RV [ ]---5v                 [ ] B1   BS [ ]---D2
+ * |       |                        |           |
+ * |   RG [ ]---GND                 +-----------+
+ * |       |
+ * +-------+
+ *
+ */
+
 #include "EffectState.hpp"
 #include "NeoPixelRing.hpp"
 
